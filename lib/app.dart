ddacd8ccffdd119c213ff285e1f883f440c59701
hendy3617 <72657840+hendy3617@users.noreply.github.com>
2021-04-21 04:40:14 +0700

    timer initial development

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:timer_bloc/counter/view/counter_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer_bloc/stopwatch/stopwatch.dart';
+import 'package:timer_bloc/timer/timer.dart';
 
-class CounterApp extends MaterialApp {
-    const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
-    
+import 'cubit/landpage_cubit.dart' as cubit;
+
+class TimerApp extends StatelessWidget {
+  const TimerApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Landpage(),
+    );
+  }
+}
+
+class Landpage extends StatelessWidget {
+  Landpage({Key? key}) : super(key: key);
+
+  final items = [
+    const BottomNavigationBarItem(
+      label: 'Stopwatch',
+      icon: Icon(Icons.lock_clock),
+    ),
+    const BottomNavigationBarItem(
+      label: 'Timer',
+      icon: Icon(Icons.timelapse),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<cubit.LandpageCubit, cubit.LandpageState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: state.currentPage == cubit.Page.stopwatch
+                ? const Text('Stopwatch')
+                : const Text('Timer'),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: items,
+            currentIndex: state.currentPage.index,
+            onTap: (value) {
+              final bloc = context.read<cubit.LandpageCubit>();
+              if (value == 0) {
+                bloc.gotoStopwatchPage();
+              } else {
+                bloc.gotoTimerPage();
+              }
+            },
+          ),
+          body: state.currentPage == cubit.Page.stopwatch
+              ? const StopwatchPage()
+              : const TimerPage(),
+        );
+      },
+    );
+  }
 }
